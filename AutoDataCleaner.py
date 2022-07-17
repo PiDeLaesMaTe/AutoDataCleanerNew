c52dfd1d203ab34852291b4ee9ec97a1955fdf85
PiDeLaesMaTe <109474099+PiDeLaesMaTe@users.noreply.github.com>
2022-07-18 01:19:53 +0200

    Update AutoDataCleaner.py

--- AutoDataCleaner.py
+++ AutoDataCleaner.py
@@ -411,21 +411,29 @@ def help():
     help_text = """
     ++++++++++++++++++ AUTO DATA CLEANER HELP +++++++++++++++++++++++
     FUNCTION CALL:
-    AutoDataCleaner.clean_me(df, one_hot=True, na_cleaner_mode="mean", normalize=True, remove_columns=[], verbose=True)
+    AutoDataCleaner.clean_me(data=adc.clean_me(df,detect_binary=True, numeric_dtype=True, decision_tree=True, one_hot=True,normalize=True,datetime_columns=[], remove_columns=['Name'], high_corr_elimination=True, low_var_elimination=True, measuring_variable='Survived', variable_to_encode=[], outlier_removal=True, duplicated_var=True, duplicated_rows_remove=True, variables_uni=True, clean=True,verbose=True))
 
     FUNCTION PARAMETERS:
-    df: input Pandas DataFrame on which the cleaning will be performed 
-    one_hot: if True, all non-numeric columns will be encoded to one-hot columns 
-    na_cleaner_mode: what technique to use when dealing with None/NA/Empty values. Modes: 
-        False: do not consider cleaning na values 
-        'remove row': removes rows with a cell that has NA value
-        'mean': substitues empty NA cells with the mean of that column  
-        'mode': substitues empty NA cells with the mode of that column
-        '*': any other value will substitute empty NA cells with that particular value passed here 
-    normalize: if True, all non-binray (columns with values 0 or 1 are excluded) columns will be normalized. 
-    remove_columns: list of columns to remove, this is usually non-related featues such as the ID column 
-    verbose: print progress in terminal/cmd
-    returns: processed and clean Pandas DataFrame 
+    df: input Pandas DataFrame on which the cleaning will be performed
+    :param dataframe: input Pandas DataFrame on which the cleaning will be performed 
+    :param detect_binray: if True, any column that has two unique values, these values will be replaced with 0 and 1 (e.g.: ['looser', 'winner'] => [0,1])
+    :param numeric_dtype: if True, columns will be converted to numeric dtypes when possible **see [1] in README.md**
+    :param decision_tree: if True, automatic detection of which variables will be transformed by decision tree and coded
+    :param one_hot: if True, all non-numeric columns will be encoded to one-hot columns 
+    :param normalize: if True, all non-binray (columns with values 0 or 1 are excluded) columns will be normalized and if True, automatic detection of which variables should be normalized 
+    :param datetime_columns: a list of columns which contains date or time or datetime entries (important to be announced in this list, otherwise hot-encoding will mess up these columns)
+    :param remove_columns: list of columns to remove, this is usually non-related featues such as the ID column 
+    :param high_corr_elimination: eliminated high corr variables (Teresa)
+    :param low_var_elimination: eliminates low variance variables (under 0.1) (Teresa)
+    :param measuring_variable: The y of the model (Teresa)
+    :param variable_to_encode: if decision_tree is true, variables that want to be changed by tree_decision
+    :param outlier_removal: if outlier_removal is True, variables will be cleaned of the outliers
+    :param duplicated_var: if duplicated_var is True, the variables will be eliminated 
+    :param duplicated_rows_remove: if duplicated_rows_remove is True,the rows that are duplicated will be eliminated
+    :param variables_uniques:if variables_uniques is True, the variables that are unique in all rows will be deleted
+    :param clean: if clean is True, the data will be cleaned of NAN Values
+    :param verbose: print progress in terminal/cmd
+    :return: processed and clean Pandas DataFrame
     ++++++++++++++++++ AUTO DATA CLEANER HELP +++++++++++++++++++++++
     """
     print(help_text)
